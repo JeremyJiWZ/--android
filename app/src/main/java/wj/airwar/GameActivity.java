f3166a4e9aede2c0c1441fc9d9fc7e6c48ab5593
JeremyJiWZ <mr.ji@icloud.com>
2015-12-10 00:31:37 +0800

    add a box, use new Interface setDirection

--- app/src/main/java/wj/airwar/GameActivity.java
+++ app/src/main/java/wj/airwar/GameActivity.java
@@ -1,5 +1,7 @@
 package wj.airwar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
@@ -16,15 +18,16 @@ import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
-    private int choosenPlane=0;//1 for plane 1, 2 for plane 2, 3 for plane 3
-    private int player=1; //player 1 or player 2
-    private int lastX,lastY;
-    private int screenWidth,screenHeight,length;
+    private int choosenPlane = 0;//1 for plane 1, 2 for plane 2, 3 for plane 3
+    private int player = 1; //player 1 or player 2
+    private int lastX, lastY;
+    private int screenWidth, screenHeight, length;
     private Airplane player1 = new Airplane();
     private Airplane player2 = new Airplane();
     ImageView plane1;
     ImageView plane2;
     ImageView plane3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,20 +36,26 @@ public class GameActivity extends AppCompatActivity {
         DisplayMetrics dm = getResources().getDisplayMetrics();
         screenWidth = dm.widthPixels;
         screenHeight = dm.heightPixels - 50;
-        length = screenHeight/14;
-        Log.i("test", "screenWidth = "+screenWidth);
-        Log.i("test", "screenHeight = "+screenHeight);
-        Log.i("test", "length = "+length);
+        length = screenHeight / 14;
+        Log.i("test", "screenWidth = " + screenWidth);
+        Log.i("test", "screenHeight = " + screenHeight);
+        Log.i("test", "length = " + length);
 
-        ImageButton spinLeft= (ImageButton)findViewById(R.id.spinLeft);
-        ImageButton spinRight = (ImageButton)findViewById(R.id.spinRight);
+        ImageButton spinLeft = (ImageButton) findViewById(R.id.spinLeft);
+        ImageButton spinRight = (ImageButton) findViewById(R.id.spinRight);
 
-        plane1 = (ImageView)findViewById(R.id.plane1);
-        plane2 = (ImageView)findViewById(R.id.plane2);
-        plane3 = (ImageView)findViewById(R.id.plane3);
+        //放置棋盘，做好布局
+        ImageView box = (ImageView)findViewById(R.id.box);
+        RelativeLayout.LayoutParams bp = new RelativeLayout.LayoutParams(length*10,length*10);
+        bp.leftMargin = length*2;
+        bp.topMargin = length*2;
+        box.setLayoutParams(bp);
 
-        RelativeLayout.LayoutParams lp =
-                new RelativeLayout.LayoutParams(length * 5, length * 5);
+        plane1 = (ImageView) findViewById(R.id.plane1);
+        plane2 = (ImageView) findViewById(R.id.plane2);
+        plane3 = (ImageView) findViewById(R.id.plane3);
+
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(length * 5, length * 5);
         lp.leftMargin = length * 12 + length / 2;
         lp.topMargin = length * 2 + length / 2;
         plane1.setLayoutParams(lp);
@@ -67,7 +76,7 @@ public class GameActivity extends AppCompatActivity {
         spinLeft.setOnClickListener(new LeftSpinListener());
         spinRight.setOnClickListener(new RightSpinListener());
 
-        Button start = (Button)findViewById(R.id.button);
+        Button start = (Button) findViewById(R.id.button);
         start.setOnClickListener(new StartListener());
 
         //initialize
@@ -85,12 +94,9 @@ public class GameActivity extends AppCompatActivity {
             int top;
             int right;
             int bottom;
-            if (v.getId()==R.id.plane1)
-                choosenPlane=1;
-            if (v.getId()==R.id.plane2)
-                choosenPlane=2;
-            if (v.getId()==R.id.plane3)
-                choosenPlane=3;
+            if (v.getId() == R.id.plane1) choosenPlane = 1;
+            if (v.getId() == R.id.plane2) choosenPlane = 2;
+            if (v.getId() == R.id.plane3) choosenPlane = 3;
 //            Log.e("choosen plane",""+choosenPlane);
             switch (action) {
                 case MotionEvent.ACTION_DOWN:
@@ -136,78 +142,86 @@ public class GameActivity extends AppCompatActivity {
                     top = v.getTop();
                     right = v.getRight();
                     bottom = v.getBottom();
-                    int row=0,col=0;
-                    v.layout(length/2+left/length*length, length/2+top/length*length,
-                            length/2+right/length*length, length/2+bottom/length*length);
+                    int row = 0, col = 0;
+                    v.layout(length / 2 + left / length * length, length / 2 + top / length * length, length / 2 + right / length * length, length / 2 + bottom / length * length);
+                    row = top/length-2;
+                    col = left/length-2;
+                    Log.e("row",""+row);
+                    Log.e("col",""+col);
                     if (player == 1) {
-                        player1.placePlane(choosenPlane - 1, row, col, player1.getDirection(choosenPlane - 1));
+                        boolean test;//for test
+                        test=player1.placePlane(choosenPlane - 1, row, col, player1.getDirection(choosenPlane - 1));
+//                        Log.e("is placed?",""+test);
                     }
                     if (player == 2)
-                        player2.placePlane(choosenPlane-1,row,col,player2.getDirection(choosenPlane-1));
+                        player2.placePlane(choosenPlane - 1, row, col, player2.getDirection(choosenPlane - 1));
                     break;
             }
-        return false;
+            return false;
         }
     }
 
     //set direction 0:up,1:right,2:down,3:left
-    private void setDir(ImageView plane,int dir){
+    private void setDir(ImageView plane, int dir) {
 
-        if(dir==0)  //up
+        if (dir == 0)  //up
             plane.setImageResource(R.drawable.plane_up);
-        if(dir==1)
-            plane.setImageResource(R.drawable.plane_right);
-        if(dir==2)
-            plane.setImageResource(R.drawable.plane_down);
-        if(dir==3)
-            plane.setImageResource(R.drawable.plane_left);
+        if (dir == 1) plane.setImageResource(R.drawable.plane_right);
+        if (dir == 2) plane.setImageResource(R.drawable.plane_down);
+        if (dir == 3) plane.setImageResource(R.drawable.plane_left);
     }
 
-
-
-    class LeftSpinListener implements ImageButton.OnClickListener{
+    class LeftSpinListener implements ImageButton.OnClickListener {
         @Override
         public void onClick(View v) {
             int dir = player1.getDirection(choosenPlane - 1);
-            dir -=1;//turn left
-            if(dir<0)//up
-                dir=3;//left
-            ImageView plane=null;
-            if (choosenPlane==1)
-                plane = (ImageView)findViewById(R.id.plane1);
-            if (choosenPlane==2)
-                plane = (ImageView)findViewById(R.id.plane2);
-            if (choosenPlane==3)
-                plane = (ImageView)findViewById(R.id.plane3);
-            if (plane !=null)
-                setDir(plane,dir);
-
+            dir -= 1;//turn left
+            if (dir < 0)//up
+                dir = 3;//left
+            ImageView plane = null;
+            if (choosenPlane == 1) plane = (ImageView) findViewById(R.id.plane1);
+            if (choosenPlane == 2) plane = (ImageView) findViewById(R.id.plane2);
+            if (choosenPlane == 3) plane = (ImageView) findViewById(R.id.plane3);
+            if (plane != null) {
+                if (player == 1) {
+                    player1.setDirection(choosenPlane-1,dir);
+                }
+                else if (player == 2) {
+                    player2.setDirection(choosenPlane-1,dir);
+                }
+                setDir(plane, dir);
+            }
         }
     }
-    class RightSpinListener implements ImageButton.OnClickListener{
+
+    class RightSpinListener implements ImageButton.OnClickListener {
         @Override
         public void onClick(View v) {
             int dir = player1.getDirection(choosenPlane - 1);
-            dir +=1;//turn right
-            if(dir>3)//left
-                dir=0;//up
-            ImageView plane=null;
-            Log.e("right spin","onclick");
-            if (choosenPlane==1)
-                plane = (ImageView)findViewById(R.id.plane1);
-            if (choosenPlane==2)
-                plane = (ImageView)findViewById(R.id.plane2);
-            if (choosenPlane==3)
-                plane = (ImageView)findViewById(R.id.plane3);
-            if (plane !=null)
-                setDir(plane,dir);
-
+            dir += 1;//turn right
+            if (dir > 3)//left
+                dir = 0;//up
+            ImageView plane = null;
+//            Log.e("right spin", "onclick");
+            if (choosenPlane == 1) plane = (ImageView) findViewById(R.id.plane1);
+            if (choosenPlane == 2) plane = (ImageView) findViewById(R.id.plane2);
+            if (choosenPlane == 3) plane = (ImageView) findViewById(R.id.plane3);
+            if (plane != null) {
+                if (player == 1) {
+                    player1.setDirection(choosenPlane-1,dir);
+                }
+                else if (player == 2) {
+                    player2.setDirection(choosenPlane-1,dir);
+                }
+                //show the rotate plane
+                setDir(plane, dir);
+            }
         }
     }
-    private void resetPlane(){
-//        pla
-        RelativeLayout.LayoutParams lp =
-                new RelativeLayout.LayoutParams(length * 5, length * 5);
+
+    private void resetPlane() {
+        //reset the plane  for player 2 to get start
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(length * 5, length * 5);
         lp.leftMargin = length * 12 + length / 2;
         lp.topMargin = length * 2 + length / 2;
         plane1.setLayoutParams(lp);
@@ -217,24 +231,54 @@ public class GameActivity extends AppCompatActivity {
         plane2.setImageResource(R.drawable.plane_up);
         plane3.setImageResource(R.drawable.plane_up);
     }
-    class StartListener implements View.OnClickListener{
+
+    class StartListener implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            if (player==1) {
-                //if(player1 planes are placed correctly)
-                player = 2;
-                resetPlane();
-                TextView textView = (TextView)findViewById(R.id.player);
-                textView.setText("Player2");
-                return;
+            if (player == 1) {
+                //if all placed, move on
+                if (player1.isAllPlaced()) {
+                    player = 2;
+                    resetPlane();
+                    TextView textView = (TextView) findViewById(R.id.player);
+                    textView.setText("Player2");
+                    return;
+                }
+                //not all placed, not move on
+                else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+                    //set title
+                    builder.setTitle("提示");
+                    //set message
+                    builder.setMessage("请将飞机放在格子内，不允许重叠");
+                    //set neutral button
+                    builder.setNeutralButton("好的", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {}
+                    });
+                    builder.show();
+                }
             }
-            if (player==2){
-                //if(player2 planes are placed correctly)
-
-                Intent intent = new Intent();
-                intent.setClass(GameActivity.this,Double_Activity.class);
-                startActivity(intent);
-                return;
+            if (player == 2) {
+                if(player2.isAllPlaced()) {
+                    Intent intent = new Intent();
+                    intent.setClass(GameActivity.this, Double_Activity.class);
+                    startActivity(intent);
+                    return;
+                }
+                else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(GameActivity.this);
+                    //set title
+                    builder.setTitle("提示");
+                    //set message
+                    builder.setMessage("请将飞机放在格子内，不允许重叠");
+                    //set neutral button
+                    builder.setNeutralButton("好的", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {}
+                    });
+                    builder.show();
+                }
             }
         }
     }
