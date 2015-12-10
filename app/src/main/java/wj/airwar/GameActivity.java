4ae78f46bb1d02b6889ae33971f7b4f72403b98f
JeremyJiWZ <mr.ji@icloud.com>
2015-12-10 18:38:44 +0800

    add index,new picture

--- app/src/main/java/wj/airwar/GameActivity.java
+++ app/src/main/java/wj/airwar/GameActivity.java
@@ -21,12 +21,12 @@ public class GameActivity extends AppCompatActivity {
     private int choosenPlane = 0;//1 for plane 1, 2 for plane 2, 3 for plane 3
     private int player = 1; //player 1 or player 2
     private int lastX, lastY;
-    private int screenWidth, screenHeight, length;
     private Airplane player1 = new Airplane();
     private Airplane player2 = new Airplane();
     ImageView plane1;
     ImageView plane2;
     ImageView plane3;
+    static public int screenWidth, screenHeight, length;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +37,12 @@ public class GameActivity extends AppCompatActivity {
         screenWidth = dm.widthPixels;
         screenHeight = dm.heightPixels - 50;
         length = screenHeight / 14;
-        Log.i("test", "screenWidth = " + screenWidth);
-        Log.i("test", "screenHeight = " + screenHeight);
-        Log.i("test", "length = " + length);
+//        Log.i("test", "screenWidth = " + screenWidth);
+//        Log.i("test", "screenHeight = " + screenHeight);
+//        Log.i("test", "length = " + length);
 
         ImageButton spinLeft = (ImageButton) findViewById(R.id.spinLeft);
         ImageButton spinRight = (ImageButton) findViewById(R.id.spinRight);
-
-        //放置棋盘，做好布局
-        ImageView box = (ImageView)findViewById(R.id.box);
-        RelativeLayout.LayoutParams bp = new RelativeLayout.LayoutParams(length*10,length*10);
-        bp.leftMargin = length*2;
-        bp.topMargin = length*2;
-        box.setLayoutParams(bp);
 
         plane1 = (ImageView) findViewById(R.id.plane1);
         plane2 = (ImageView) findViewById(R.id.plane2);
@@ -83,6 +76,101 @@ public class GameActivity extends AppCompatActivity {
         plane1.setImageResource(R.drawable.plane_up);
         plane2.setImageResource(R.drawable.plane_up);
         plane3.setImageResource(R.drawable.plane_up);
+
+        createBox();
+    }
+    private void createBox(){
+        //放置棋盘，做好布局
+        ImageView box = (ImageView)findViewById(R.id.box);
+        RelativeLayout.LayoutParams bp = new RelativeLayout.LayoutParams(length*10,length*10);
+        bp.leftMargin = length*2;
+        bp.topMargin = length*2;
+        box.setLayoutParams(bp);
+        //art number 行
+        ImageView one = (ImageView)findViewById(R.id.one);
+        ImageView two = (ImageView)findViewById(R.id.two);
+        ImageView three = (ImageView)findViewById(R.id.three);
+        ImageView four = (ImageView)findViewById(R.id.four);
+        ImageView five = (ImageView)findViewById(R.id.five);
+        ImageView six = (ImageView)findViewById(R.id.six);
+        ImageView seven = (ImageView)findViewById(R.id.seven);
+        ImageView eight = (ImageView)findViewById(R.id.eight);
+        ImageView nine = (ImageView)findViewById(R.id.nine);
+        ImageView ten = (ImageView)findViewById(R.id.ten);
+
+        RelativeLayout.LayoutParams ly_1 = new RelativeLayout.LayoutParams(length,length);
+        ly_1.leftMargin = length*2; ly_1.topMargin=length;
+        one.setLayoutParams(ly_1);
+        RelativeLayout.LayoutParams ly_2 = new RelativeLayout.LayoutParams(length,length);
+        ly_2.leftMargin = length*3; ly_2.topMargin=length;
+        two.setLayoutParams(ly_2);
+        RelativeLayout.LayoutParams ly_3 = new RelativeLayout.LayoutParams(length,length);
+        ly_3.leftMargin = length*4; ly_3.topMargin=length;
+        three.setLayoutParams(ly_3);
+        RelativeLayout.LayoutParams ly_4 = new RelativeLayout.LayoutParams(length,length);
+        ly_4.leftMargin = length*5; ly_4.topMargin=length;
+        four.setLayoutParams(ly_4);
+        RelativeLayout.LayoutParams ly_5 = new RelativeLayout.LayoutParams(length,length);
+        ly_5.leftMargin = length*6; ly_5.topMargin=length;
+        five.setLayoutParams(ly_5);
+        RelativeLayout.LayoutParams ly_6 = new RelativeLayout.LayoutParams(length,length);
+        ly_6.leftMargin = length*7; ly_6.topMargin=length;
+        six.setLayoutParams(ly_6);
+        RelativeLayout.LayoutParams ly_7 = new RelativeLayout.LayoutParams(length,length);
+        ly_7.leftMargin = length*8; ly_7.topMargin=length;
+        seven.setLayoutParams(ly_7);
+        RelativeLayout.LayoutParams ly_8 = new RelativeLayout.LayoutParams(length,length);
+        ly_8.leftMargin = length*9; ly_8.topMargin=length;
+        eight.setLayoutParams(ly_8);
+        RelativeLayout.LayoutParams ly_9 = new RelativeLayout.LayoutParams(length,length);
+        ly_9.leftMargin = length*10; ly_9.topMargin=length;
+        nine.setLayoutParams(ly_9);
+        RelativeLayout.LayoutParams ly_10 = new RelativeLayout.LayoutParams(length,length);
+        ly_10.leftMargin = length*11; ly_10.topMargin=length;
+        ten.setLayoutParams(ly_10);
+
+        //art number 列
+        ImageView one_col = (ImageView)findViewById(R.id.one_col);
+        ImageView two_col = (ImageView)findViewById(R.id.two_col);
+        ImageView three_col = (ImageView)findViewById(R.id.three_col);
+        ImageView four_col = (ImageView)findViewById(R.id.four_col);
+        ImageView five_col = (ImageView)findViewById(R.id.five_col);
+        ImageView six_col = (ImageView)findViewById(R.id.six_col);
+        ImageView seven_col = (ImageView)findViewById(R.id.seven_col);
+        ImageView eight_col = (ImageView)findViewById(R.id.eight_col);
+        ImageView nine_col = (ImageView)findViewById(R.id.nine_col);
+        ImageView ten_col = (ImageView)findViewById(R.id.ten_col);
+
+        RelativeLayout.LayoutParams ly_1_col = new RelativeLayout.LayoutParams(length,length);
+        ly_1_col.leftMargin = length; ly_1_col.topMargin=length*2;
+        one_col.setLayoutParams(ly_1_col);
+        RelativeLayout.LayoutParams ly_2_col = new RelativeLayout.LayoutParams(length,length);
+        ly_2_col.leftMargin = length; ly_2_col.topMargin=length*3;
+        two_col.setLayoutParams(ly_2_col);
+        RelativeLayout.LayoutParams ly_3_col = new RelativeLayout.LayoutParams(length,length);
+        ly_3_col.leftMargin = length; ly_3_col.topMargin=length*4;
+        three_col.setLayoutParams(ly_3_col);
+        RelativeLayout.LayoutParams ly_4_col = new RelativeLayout.LayoutParams(length,length);
+        ly_4_col.leftMargin = length; ly_4_col.topMargin=length*5;
+        four_col.setLayoutParams(ly_4_col);
+        RelativeLayout.LayoutParams ly_5_col = new RelativeLayout.LayoutParams(length,length);
+        ly_5_col.leftMargin = length; ly_5_col.topMargin=length*6;
+        five_col.setLayoutParams(ly_5_col);
+        RelativeLayout.LayoutParams ly_6_col = new RelativeLayout.LayoutParams(length,length);
+        ly_6_col.leftMargin = length; ly_6_col.topMargin=length*7;
+        six_col.setLayoutParams(ly_6_col);
+        RelativeLayout.LayoutParams ly_7_col = new RelativeLayout.LayoutParams(length,length);
+        ly_7_col.leftMargin = length; ly_7_col.topMargin=length*8;
+        seven_col.setLayoutParams(ly_7_col);
+        RelativeLayout.LayoutParams ly_8_col = new RelativeLayout.LayoutParams(length,length);
+        ly_8_col.leftMargin = length; ly_8_col.topMargin=length*9;
+        eight_col.setLayoutParams(ly_8_col);
+        RelativeLayout.LayoutParams ly_9_col = new RelativeLayout.LayoutParams(length,length);
+        ly_9_col.leftMargin = length; ly_9_col.topMargin=length*10;
+        nine_col.setLayoutParams(ly_9_col);
+        RelativeLayout.LayoutParams ly_10_col = new RelativeLayout.LayoutParams(length,length);
+        ly_10_col.leftMargin = length; ly_10_col.topMargin=length*11;
+        ten_col.setLayoutParams(ly_10_col);
 
     }
 
@@ -144,10 +232,10 @@ public class GameActivity extends AppCompatActivity {
                     bottom = v.getBottom();
                     int row = 0, col = 0;
                     v.layout(length / 2 + left / length * length, length / 2 + top / length * length, length / 2 + right / length * length, length / 2 + bottom / length * length);
-                    row = top/length-1;
+                    row = top/length-2;
                     col = left/length-2;
-                    Log.e("row",""+row);
-                    Log.e("col",""+col);
+//                    Log.e("row",""+row);
+//                    Log.e("col",""+col);
                     if (player == 1) {
                         boolean test;//for test
                         test=player1.placePlane(choosenPlane - 1, row, col, player1.getDirection(choosenPlane - 1));
@@ -242,6 +330,8 @@ public class GameActivity extends AppCompatActivity {
                     resetPlane();
                     TextView textView = (TextView) findViewById(R.id.player);
                     textView.setText("Player2");
+                    TextView tips = (TextView)findViewById(R.id.tips);
+                    tips.setText("请交给玩家2摆放");
                     return;
                 }
                 //not all placed, not move on
